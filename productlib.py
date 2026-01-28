6e5989c122127478328b3273420f7f9ad6489dca
MartinFLL <116529734+MartinFLL@users.noreply.github.com>
2026-01-27 22:23:24 -0300

    Delete productlib.py

--- productlib.py
+++
@@ -1,9 +0,0 @@
-class Product:
-    def __init__(self, id, name, category, price):
-        self.id = id
-        self.name = name
-        self.category = category
-        self.price = price
-
-rtx = Product(1, "RTX", "tech", 999979)
-rtx = Product(2, "CPU", "tech", 300000)
